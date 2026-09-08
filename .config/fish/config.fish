32763f24e776e1be98b79efa39fab4a8d414a2f7
arismav <aris.mav@hotmail.com>
2026-09-08 21:07:06 +0200

    formatting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,14 +2,14 @@
 if status is-interactive
 
     fish_vi_key_bindings
-    set fish_cursor_default     block      
-    set fish_cursor_insert      line       blink
+    set fish_cursor_default block
+    set fish_cursor_insert line blink
     set fish_cursor_replace_one underscore blink
-    set fish_cursor_visual      block
+    set fish_cursor_visual block
 
-    set fish_greeting 
+    set fish_greeting
 
-    if type -q zoxide 
+    if type -q zoxide
         zoxide init fish | source
     end
 end
